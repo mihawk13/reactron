70d25f5fef7c11598d8d384fd596feae482c16b8
bbDev <bbdev13@gmail.com>
2021-02-14 17:25:34 +0800

    first commit

---
+++ public/electron.js
@@ -0,0 +1,55 @@
+const { app, BrowserWindow } = require("electron");
+
+const path = require("path");
+const isDev = require("electron-is-dev");
+
+let mainWindow;
+
+async function createWindow() {
+  if (isDev) {
+    try {
+      const {
+        default: installExtension,
+        REACT_DEVELOPER_TOOLS,
+      } = require("electron-devtools-installer");
+      const name = await installExtension(REACT_DEVELOPER_TOOLS, true);
+      console.log(name, "was installed");
+    } catch (error) {}
+  }
+  mainWindow = new BrowserWindow({
+    width: 1050,
+    height: 625,
+    show: false,
+    icon: path.join(
+      isDev ? process.cwd() + "/resources" : process.resourcesPath,
+      "media",
+      "icon.ico"
+    ),
+    webPreferences: {
+        webSecurity: true,
+        contextIsolation: true,
+        nodeIntegration: false,
+        nativeWindowOpen: true
+    }
+  });
+  mainWindow.on("ready-to-show", async () => {
+    mainWindow.show();
+    if (isDev) mainWindow.webContents.openDevTools({ mode: "undocked" });
+  });
+  mainWindow.on("closed", () => (mainWindow = null));
+  await mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+}
+
+app.on("ready", createWindow);
+
+app.on("activate", () => {
+  if (mainWindow === null) createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
